243b891357320ce82a243ff78f720231a9bfdf26
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-06-16 11:58:26 -0700

    cleeaned up code - makeBoard() added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,57 @@
 
 
 $(()=>{
-
+  //global variables
   let alternate = true
-  let $box = $('.box')
+  let $box = $('.square')
+  let $gameContainer = $('.game-container')
 
+  //Click event
   const $clicked = (event)=>{
     const $clickedBox = $(event.target)
+      //uses global variable to switch from on player to another
       if (alternate === true) {
         $clickedBox
+        //assign background image to show gamepiece
         .css('background-image', 'url(img/Death-Eater-gamepiece.jpg)')
-        // console.log('ive been clicked!');
+        //add class to assign box to player who clicked
+        .addClass('deathEaterPlay')
+        //prevent change of game piece once played
         alternate = false
+        //turn off click so gamepiece cannot be changed
         $clickedBox.off('click')
       } else {
         $clickedBox
         .css('background-image', 'url(img/oop-gamepiece2.jpg)')
+        .addClass('oOPPlay')
         alternate = true
         $clickedBox.off('click')
       }
+      checkWin();
   }
 
+  const makeBoard = ()=>{
+    for (let i = 0; i < 36; i++){
+      $gameContainer.append(
+        $('<div>')
+        .addClass('square')
+        .attr('id', 'box'+i)
+        .on('click', $clicked)
+      )
+    }
+  }
 
   $box.on('click', $clicked)
 
+  // to check win
+  // create a function to check to see if adjacent rows have matching classes
+  // have to have four matching classes in a row
+  // create winner variable (let) and change to either deathEater or oOP
+
+  // const checkWin = ()=>{
+  //     const sibs = $('.box').siblings('.deathEaterPlay')
+  //     console.log(sibs);
+  // }
 
 
 
@@ -31,12 +59,7 @@ $(()=>{
 
 
 
-
-
-
-
-
-
+  makeBoard()
 
 
 })
