c7118f6a2941bfe5572dd79e6ad48a20adbcd488
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-06-16 10:05:04 -0700

    click event working

--- js/app.js
+++ js/app.js
@@ -2,8 +2,19 @@
 
 $(()=>{
 
+  // let alternate = true;  border: 1px solid orange;
+  let $box = $('.box')
 
-  
+  const $clicked = (event)=>{
+    const $clickedBox = $(event.target)
+      $clickedBox
+        .css({'background-image': 'img/death_eaters.jpg'})
+
+      console.log('ive been clicked!');
+  }
+
+
+  $box.on('click', $clicked)
 
 
 
