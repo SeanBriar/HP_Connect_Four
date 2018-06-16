d963b34936173f4dd09a0b0e44d708d024f5a62c
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-06-16 14:25:32 -0700

    reworked makeBoard

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 $(()=>{
   //global variables
+  let rows = 6
+  let cols = 7
   let alternate = true
   const $box = $('.square')
   const $gameContainer = $('.game-container')
@@ -26,6 +28,7 @@ $(()=>{
   }
 
   //Reset Button
+  //on click remove all added classes/id's on gamepiece div's that occurred when player clicked box div
   const $resetBtn = $('<button>').text('Reset the game').addClass('btn')
   $('#aside-content').append($resetBtn)
   $resetBtn.on('click', ()=>{
@@ -37,26 +40,28 @@ $(()=>{
 
   //Making the game board
   const makeBoard = ()=>{
-    //make loop that adds a 6x6 grid to game container
-    for (let i = 0; i < 36; i++){
-      $gameContainer.append(
-        $('<div>')
-        //add class for easy grabbing
-        .addClass('square')
-        //add id for individual numbers
-        .attr('id', 'box'+i)
-        //add onClick function
-        .on('click', $clicked)
-      )
+    //make loops that adds a 6x7 grid to game container
+    //first loop to make rows and append to container
+    for (let row = 0; row < rows; row++){
+      const $row = $('<div>').addClass('row')
+      // now create a loop to make columns and append to the rows
+      for (let col = 0; col < cols; col++) {
+        const $col = $('<div>')
+          $col
+          //add class for easy grabbing
+          .addClass('square')
+          //////// add id for individual numbers ////////////
+          //add onClick function
+          .on('click', $clicked)
+          $row.append($col)
+      }
+      $gameContainer.append($row)
     }
   }
 
-  //Reset Button
-  //on click remove all added classes/id's on gamepiece div's that occurred when player clicked box div
-
-
-
-
+  // Valid boxes
+  // If a box has has a piece below it, it can be clicked and played
+  // If a box does not have a piece below it, it is not valid for play
 
   // To Check Win
   // create a function to check to see if adjacent rows have matching classes
