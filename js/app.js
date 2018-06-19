5e6ea99dd27402c1bc64eeac73baf82091644c81
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-06-19 09:46:04 -0700

    reworking game. New board, new win logic.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ $(()=>{
   //global variables
   let alternate = true
   let currentPlayer = 1
-  let gameCounter = 0
-  let maxWin = 3
-  let rowNum = 6
-  let colNum = 6
+  // let gameCounter = 0
+  // let maxWin = 3
+  // let rowNum = 6
+  // let colNum = 6
   const $gameContainer = $('.game-container')
 
 
@@ -28,7 +28,6 @@ $(()=>{
       //prevent change of game piece once played
       alternate = false
       //turn off click so gamepiece cannot be changed
-      match = true
     } else {
       $clickedBox
       .addClass('oOPPlay')
@@ -36,94 +35,107 @@ $(()=>{
       // .off()
       currentPlayer = 2
       alternate = true
-      match = true
     }
+  } // ends click event function
 
-    // To Check Win
-    //on win activate modal with win phrase "DE won!" or "OoP won!"
-    const calculateWin = ()=>{
-      if (gameCounter >= maxWin) {
-        console.log(gameCounter);
-        console.log('winner');
-        alert('someone has one!')
-        // enable modus
-        // enter winner info
-      } else {
-        console.log('no winner yet');
-        // continue play
-        //reset game gameCounter
-        gameCounter = 0
+    const makeBoard = () => {
+      for (let i = 0; i < 36; i++){
+        $gameContainer.append(
+          $('<div>')
+          .addClass('square')
+          .attr('id', 'box'+i)
+          .on('click', $clicked)
+        )
       }
     }
-    // have to have four matching classes in a row
-    // create winner variable (let) and change to either deathEater or oOP
-    const checkForWin = ($clickedBox)=> {
-      console.log($clickedBox);
-      console.log('check for win running');
 
-      // cyle through the boxes until
-      //horizonal wins
-      checkNextBox($clickedBox, 0, 1) //left: add one column
-      checkNextBox($clickedBox, 0 -1) //right: subtract one column
-      calculateWin()
+  //   // To Check Win
+  //   //on win activate modal with win phrase "DE won!" or "OoP won!"
+  //   const calculateWin = ()=>{
+  //     if (gameCounter >= maxWin) {
+  //       console.log(gameCounter);
+  //       console.log('winner');
+  //       alert('someone has one!')
+  //       // enable modus
+  //       // enter winner info
+  //     } else {
+  //       console.log('no winner yet');
+  //       // continue play
+  //       //reset game gameCounter
+  //       gameCounter = 0
+  //     }
+  //   }
+  //   // have to have four matching classes in a row
+  //   // create winner variable (let) and change to either deathEater or oOP
+  //   const checkForWin = ($clickedBox)=> {
+  //     console.log($clickedBox);
+  //     console.log('check for win running');
+  //
+  //     // cyle through the boxes until
+  //     //horizonal wins
+  //     checkNextBox($clickedBox, 0, 1) //left: add one column
+  //     checkNextBox($clickedBox, 0 -1) //right: subtract one column
+  //     calculateWin()
+  //
+  //
+  //     // vertical wins
+  //     checkNextBox($clickedBox, 1, 0) //up
+  //     checkNextBox($clickedBox, -1, 0) //down
+  //     calculateWin()
+  //
+  //     let boxCurRow = 0
+  //     let boxCurCol = 0
+  //   } //ends checForkWin
+  //
+  //     // create a function to check to see if adjacent rows have matching classes
+  //   const checkNextBox = ($clickedBox, row, col)=>{
+  //     // console.log('starting check next box');
+  //     //get current cell position
+  //     let boxCurRow = Number($clickedBox.attr('row'))
+  //     let boxCurCol = Number($clickedBox.attr('col'))
+  //     // console.log('current clicked box row: ' + boxCurRow + ', ' + 'current clicked box col: ' + boxCurCol);
+  //     // find next cell overflow
+  //     let nextRowNum = boxCurRow+row
+  //     let nextColNum = boxCurCol+col
+  //     let $nextBox = $(`div[row=${nextRowNum}][col=${nextColNum}]`)
+  //     console.log('the next box is: ' , $nextBox);
+  //
+  //     // loop to check boxes
+  //     while (
+  //       (nextRowNum > 0) && (nextColNum > 0) && (nextRowNum <= rowNum) && (nextColNum <= colNum) && (match) && (gameCounter <= maxWin)){
+  //         // console.log('starting while loop');
+  //         // console.log('current player is: ' , currentPlayer)
+  //         // console.log('next box is ' , $nextBox);
+  //         if ($nextBox.attr('player') == currentPlayer) {
+  //           // console.log('matched');
+  //           gameCounter ++
+  //
+  //           //check next cell (depends on direction)
+  //           nextRowNum += row
+  //           nextColNum += col
+  //           $nextBox = $(`div[row=${nextRowNum}][col=${nextColNum}]`)
+  //           console.log('next box is: ' , $nextBox);
+  //         } else {
+  //           console.log('no match');
+  //           match = false
+  //           gameCounter = 0
+  //         }
+  //       } //ends whileloop
+  //
+  //   //
+  //   //
+  //   // }
+  //   //
+  // } //ends checkNextBox function
+  //
+  //
+  //
+  // checkForWin($clickedBox)
+
+    // if ()
 
 
-      // vertical wins
-      checkNextBox($clickedBox, 1, 0) //up
-      checkNextBox($clickedBox, -1, 0) //down
-      calculateWin()
-
-      let boxCurRow = 0
-      let boxCurCol = 0
-    } //ends checForkWin
-
-      // create a function to check to see if adjacent rows have matching classes
-    const checkNextBox = ($clickedBox, row, col)=>{
-      // console.log('starting check next box');
-      //get current cell position
-      let boxCurRow = Number($clickedBox.attr('row'))
-      let boxCurCol = Number($clickedBox.attr('col'))
-      // console.log('current clicked box row: ' + boxCurRow + ', ' + 'current clicked box col: ' + boxCurCol);
-      // find next cell overflow
-      let nextRowNum = boxCurRow+row
-      let nextColNum = boxCurCol+col
-      let $nextBox = $(`div[row=${nextRowNum}][col=${nextColNum}]`)
-      console.log('the next box is: ' , $nextBox);
-
-      // loop to check boxes
-      while (
-        (nextRowNum > 0) && (nextColNum > 0) && (nextRowNum <= rowNum) && (nextColNum <= colNum) && (match) && (gameCounter <= maxWin)){
-          // console.log('starting while loop');
-          // console.log('current player is: ' , currentPlayer)
-          // console.log('next box is ' , $nextBox);
-          if ($nextBox.attr('player') == currentPlayer) {
-            // console.log('matched');
-            gameCounter ++
-
-            //check next cell (depends on direction)
-            nextRowNum += row
-            nextColNum += col
-            $nextBox = $(`div[row=${nextRowNum}][col=${nextColNum}]`)
-            console.log('next box is: ' , $nextBox);
-          } else {
-            console.log('no match');
-            match = false
-            gameCounter = 0
-          }
-        } //ends whileloop
-
-    //
-    //
-    // }
-    //
-  } //ends checkNextBox function
-
-
-
-  checkForWin($clickedBox)
-
-  } // ends click event function
-  $('.square').on('click', $clicked)
+  // $('.square').on('click', $clicked)
 
 
   //Reset Button
@@ -140,7 +152,7 @@ $(()=>{
   // If a box has has a piece below it, it can be clicked and played
   // If a box does not have a piece below it, it is not valid for play
 
-
+  makeBoard()
 }) //document ready
 
 
