3309f41f88a11d756e0bde390861878ba2da5367
Sean <seanoneill@Bons-MacBook-Pro.local>
2018-06-19 17:20:30 -0700

    modal is perfect.

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ $(()=>{
   // Check for Winner
   const checkWin = () => {
     //if there are four of the same classes in a row, there is a winner
-    const $winText = $('#wintext')
+    const $winText = $('#winText')
     const $winImg = ('#winImage')
     // set modul display to 'block' to display winner
     // add close button on modal
@@ -65,80 +65,80 @@ $(()=>{
       winner = 2
 
     } else if ($('#box31').hasClass('deathEaterPlay') && ($('#box32').hasClass('deathEaterPlay')) && $('#box33').hasClass('deathEaterPlay') && ($('#box34').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box31').hasClass('oOPPlay') && ($('#box32').hasClass('oOPPlay')) && $('#box33').hasClass('oOPPlay') && ($('#box34').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
     } else if ($('#box32').hasClass('deathEaterPlay') && ($('#box33').hasClass('deathEaterPlay')) && $('#box34').hasClass('deathEaterPlay') && ($('#box35').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box32').hasClass('oOPPlay') && ($('#box33').hasClass('oOPPlay')) && $('#box34').hasClass('oOPPlay') && ($('#box35').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
     // row 5
     } else if ($('#box24').hasClass('deathEaterPlay') && ($('#box25').hasClass('deathEaterPlay')) && $('#box26').hasClass('deathEaterPlay') && ($('#box27').hasClass('deathEaterPlay'))) {
-      console.log('deathEater wins');
+        winner = 1
     } else if ($('#box24').hasClass('oOPPlay') && ($('#box25').hasClass('oOPPlay')) && $('#box26').hasClass('oOPPlay') && ($('#box27').hasClass('oOPPlay'))){
-      console.log('order wins');
+      winner = 2
 
     } else if ($('#box25').hasClass('deathEaterPlay') && ($('#box26').hasClass('deathEaterPlay')) && $('#box27').hasClass('deathEaterPlay') && ($('#box28').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box25').hasClass('oOPPlay') && ($('#box26').hasClass('oOPPlay')) && $('#box27').hasClass('oOPPlay') && ($('#box28').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
     } else if ($('#box32').hasClass('deathEaterPlay') && ($('#box33').hasClass('deathEaterPlay')) && $('#box34').hasClass('deathEaterPlay') && ($('#box35').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box32').hasClass('oOPPlay') && ($('#box33').hasClass('oOPPlay')) && $('#box34').hasClass('oOPPlay') && ($('#box35').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
     //row 4
     } else if ($('#box18').hasClass('deathEaterPlay') && ($('#box19').hasClass('deathEaterPlay')) && $('#box20').hasClass('deathEaterPlay') && ($('#box21').hasClass('deathEaterPlay'))) {
-      console.log('deathEater wins');
+        winner = 1
     } else if ($('#box18').hasClass('oOPPlay') && ($('#box19').hasClass('oOPPlay')) && $('#box26').hasClass('oOPPlay') && ($('#box27').hasClass('oOPPlay'))){
-      console.log('order wins');
+      winner = 2
 
     } else if ($('#box19').hasClass('deathEaterPlay') && ($('#box20').hasClass('deathEaterPlay')) && $('#box21').hasClass('deathEaterPlay') && ($('#box22').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box19').hasClass('oOPPlay') && ($('#box20').hasClass('oOPPlay')) && $('#box21').hasClass('oOPPlay') && ($('#box22').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
     } else if ($('#box32').hasClass('deathEaterPlay') && ($('#box33').hasClass('deathEaterPlay')) && $('#box34').hasClass('deathEaterPlay') && ($('#box35').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box32').hasClass('oOPPlay') && ($('#box33').hasClass('oOPPlay')) && $('#box34').hasClass('oOPPlay') && ($('#box35').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
 
       //row 3
     } else if ($('#box12').hasClass('deathEaterPlay') && ($('#box13').hasClass('deathEaterPlay')) && $('#box14').hasClass('deathEaterPlay') && ($('#box15').hasClass('deathEaterPlay'))) {
-      console.log('deathEater wins');
+        winner = 1
     } else if ($('#box12').hasClass('oOPPlay') && ($('#box13').hasClass('oOPPlay')) && $('#box14').hasClass('oOPPlay') && ($('#box15').hasClass('oOPPlay'))){
-      console.log('order wins');
+      winner = 2
 
     } else if ($('#box13').hasClass('deathEaterPlay') && ($('#box14').hasClass('deathEaterPlay')) && $('#box15').hasClass('deathEaterPlay') && ($('#box16').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box13').hasClass('oOPPlay') && ($('#box14').hasClass('oOPPlay')) && $('#box15').hasClass('oOPPlay') && ($('#box16').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
     } else if ($('#box14').hasClass('deathEaterPlay') && ($('#box15').hasClass('deathEaterPlay')) && $('#box16').hasClass('deathEaterPlay') && ($('#box17').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box14').hasClass('oOPPlay') && ($('#box15').hasClass('oOPPlay')) && $('#box16').hasClass('oOPPlay') && ($('#box17').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
       }
 
       //row 2
      else if ($('#box6').hasClass('deathEaterPlay') && ($('#box7').hasClass('deathEaterPlay')) && $('#box8').hasClass('deathEaterPlay') && ($('#box9').hasClass('deathEaterPlay'))) {
-      console.log('deathEater wins');
+        winner = 1
     } else if ($('#box6').hasClass('oOPPlay') && ($('#box7').hasClass('oOPPlay')) && $('#box8').hasClass('oOPPlay') && ($('#box9').hasClass('oOPPlay'))){
-      console.log('order wins');
+      winner = 2
 
     } else if ($('#box7').hasClass('deathEaterPlay') && ($('#box8').hasClass('deathEaterPlay')) && $('#box9').hasClass('deathEaterPlay') && ($('#box10').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box7').hasClass('oOPPlay') && ($('#box8').hasClass('oOPPlay')) && $('#box9').hasClass('oOPPlay') && ($('#box10').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
 
     } else if ($('#box8').hasClass('deathEaterPlay') && ($('#box9').hasClass('deathEaterPlay')) && $('#box10').hasClass('deathEaterPlay') && ($('#box11').hasClass('deathEaterPlay'))){
-      console.log('deathEater wins');
+        winner = 1
     }else if ($('#box8').hasClass('oOPPlay') && ($('#box9').hasClass('oOPPlay')) && $('#box10').hasClass('oOPPlay') && ($('#box11').hasClass('oOPPlay'))){
-        console.log('order wins');
+        winner = 2
       }
 
 
@@ -152,7 +152,7 @@ $(()=>{
         $('<img id="deWinPic" src="https://vignette.wikia.nocookie.net/nicktheultimaswordwielder/images/1/12/Lord_Voldemort_%26_the_Death_Eaters.jpg/revision/latest?cb=20130823032305" />').appendTo($winImg)
       } else if (winner === 2) {
         $modal.css('display', 'block')
-        $winText.text('The Order of the Pheonix have successfuly defended Hogwarts!')
+        $winText.text('The Order of the Pheonix have defended Hogwarts!')
         $('<img id="oopWinPic" src="http://cdn.playbuzz.com/cdn/41929c94-eab4-4a37-8d21-01ab35c3c384/a81010e7-c0ce-4998-80be-8a0ae6c6d709.jpg" />').appendTo($winImg)
       }
   } // ends checkWin
